4d2d1ccae2d99b00bb4bf88b45df397cc38ce164
Wahyudwikuncoro220103 <106148639+Wahyudwikuncoro220103@users.noreply.github.com>
2023-05-26 16:25:29 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+//alert('SELAMAT DATANG DI WEBSITE PORTFOLIO WAHYU DWI KUNCORO')
+
+let nama = 'Wahyu Dwi Kuncoro'
+let nim = '2155201204'
+let tempattanggallahir = 'Tangerang, 22 Januari 2003'
+let jeniskelamin = 'Laki Laki'
+let nohandphone = '087872671664'
+let gmail = 'wahyuu.kuncoroo22@gmail.com'
+let alamat = 'Kp.Karang Sari RT.002/RW.012 No.59 Kota Tangerang'
+let programstudi = 'Teknik Informatika'
+
+alert(
+ `SELAMAT DATANG DI WEBSITE PORTFOLIO WAHYU DWI KUNCORO
+_________________________________________________________________________
+UNIVERSITAS MUHAMMADIYAH TANGERANG
+NAMA                       : ${nama}
+NIM                           : ${nim}
+TEMPAT TGL LAHIR   : ${tempattanggallahir}
+JENIS KELAMIN         : ${jeniskelamin}
+NO.HANDPHONE      : ${nohandphone}
+GMAIL                        : ${gmail}
+ALAMAT                     : ${alamat}
+PROGRAM STUDI       : ${programstudi}`
+)
